91f9e3b203b356c24e60c8771267b91da8baf92b
Adriana Kramer <adri.kramer@gmail.com>
2022-09-03 15:03:43 +0200

    Add tests in board_spec to check winning row

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,26 @@
-#frozen_string_literal: true
-#board_spec.rb
+# frozen_string_literal: true
 
 require_relative '../lib/board'
 
 describe Board do
-  subject(:board) { described_class.new() }
+  subject(:board) { described_class.new }
+
+  describe '#winner?(symbol)' do
+
+    context 'when winner uses symbol X and has a winning row' do
+      before do
+        board.instance_variable_set(:@board, [[1, 'O', 3], %w[X X X], [7, 'O', 9]])
+      end
+
+      it "results in true when called with symbol 'X'" do
+        winner_result = board.winner?('X')
+        expect(winner_result).to be true
+      end
+
+      it "results in false when called with symbol 'O'" do
+        winner_result = board.winner?('O')
+        expect(winner_result).to be false
+      end
+    end
+  end
 end
