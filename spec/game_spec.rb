eea3732f6e07b0f1af66fc38cd3ffcb184b931ec
Adriana Kramer <adri.kramer@gmail.com>
2022-09-04 16:47:58 +0200

    Set up subject and instance variables in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/player'
 
 describe Game do
+  subject(:game) { described_class.new }
+  let(:player_one) { instance_double(Player) }
+  let(:player_two) { instance_double(Player) }
 
+  describe '#play_game' do
+    # TODO
+    # Test to make sure that loop ends when conditions are met
+    # Test to make sure that messages are sent
+  end
+
+  describe '#determine_current_player' do
+    # When players are initialized, player 1 is the first player -> no need to set up any instance variables before testing
+  end
 end
